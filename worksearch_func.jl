8d13b56a30232adc5bc41f06399f4ef3b2385f9d
Cory Briar <cbriar@uoregon.edu>
2019-11-13 19:04:23 -0800

    Beginning trouble shooting, made all matricies JxI

--- worksearch_func.jl
+++ worksearch_func.jl
@@ -32,7 +32,7 @@ function τ(para, firms)
         locs[:,l] .= l
     end # l-loop
     # Create matrix of travel costs
-    tcosts = τ1 .+ τ2.*abs.(locs .- transpose(firms))
+    tcosts = τ1 .+ τ2.*abs.(locs .- firms)
     return tcosts
 end #τ
 
@@ -43,21 +43,23 @@ function γ(para)
     return commcosts
 end # γ
 
-function wage_eq(para, Kl, rents, hhs, hl, firms, fl, offers_guess, prices, order, Θ, it_tols)
+function wage_eq(para, Kl, hhs, hl, firms, fl, offers_guess, prices, Θ, it_tols)
     @unpack I, J, L, σ1, σ2, ψ, γ1, γ2, κ, α = para
     # Calculate firm demands
-    Z = σ1*σ2.*exp.(-σ1.*(transpose(prices) .+ τ(para,firms)))*transpose(hl)
+    Z = σ1*σ2.*exp.(-σ1.*(prices .+ τ(para,firms)))*hl
 
     # initialize while loop
     iter = 0
     maxdiff = 1e+6
     offers = offers_guess
-
+    matches_stor = zeros(J,I,Int(it_tols[1])+1)
+    offers_stor = zeros(J,I,2,Int(it_tols[1])+1)
+    offers_stor[:,:,:,1] = offers_guess
     # while loop to obtain optimal firm offers (wage equilibrium)
     while (maxdiff > it_tols[2]) & (iter < it_tols[1] + 1)
         iter += 1
         # Compile matrix of potential ω's for each household
-        ω_ij = (offers[:,:,1] - γ(para)[hhs,firms]).*offers[:,:,2] - ψ.*offers[:,:,2].^2
+        ω_ij = (offers[:,:,1] - γ(para)[firms,hhs]).*offers[:,:,2] - ψ.*offers[:,:,2].^2
         # If greater than 0, hh's apply to job (i,j)
         applications = ω_ij .> 0
         # Firms choose which hh's to hire
@@ -66,7 +68,7 @@ function wage_eq(para, Kl, rents, hhs, hl, firms, fl, offers_guess, prices, orde
             # Find households that applied to j
             Ωaj = zeros(0)
             for i in 1:I
-                if application[i,j] == true
+                if applications[j,i] == true
                     append!(Ωaj, Int(i))
                 end # appending if statement
             end # i-loop
@@ -78,7 +80,7 @@ function wage_eq(para, Kl, rents, hhs, hl, firms, fl, offers_guess, prices, orde
             Nj_costs = zeros(0)
             for x in 1:size(all_plans)[2]
                 # If output of plan x >= Nj, append cost to Nj_costs
-                if sum(Θ[all_plans[x]].*offers[all_plans[x],j,2]) >= N_j
+                if sum(Θ[all_plans[x]].*log.(offers[all_plans[x],j,2])) >= N_j
                     append!(Nj_costs, sum(offers[all_plans[x],j,1].*offers[all_plans[x],j,2]))
                 end
             end # x-loop
@@ -92,25 +94,37 @@ function wage_eq(para, Kl, rents, hhs, hl, firms, fl, offers_guess, prices, orde
 
         # Households accept best offer
         matches = zeros(J,I)
-        hires = transpose(ω_ij) .* Ωhj
+        hire_offers = transpose(ω_ij) .* Ωhj
         for i in 1:I
-            matches[findmax(hires[:,i]),i] = 1
+            matches[findmax(hire_offers[:,i]),i] = 1
         end # i-loop
 
+        # Create vector to store households matched to each j
+        Ω_star = zeros(0)
+        for j in 1:J
+            Ω_star[j] = findall(i -> i>0,matches[j,:])
+        end # j-loop
+
+        # Update offers
+        for j in 1:J
+            for i in 1:I
+                if (matches[j,i] = 1) & (Ωhj[j,i] = 1)
+                    # Set optimal offer
+                    offers[j,i,2] = exp((N_j[j] - sum(Θ[j,Ω_star[j]].*log.(Θ[j,Ω_star[j]].*offers[j,i,1]./Θ[j,i].*offers[j,:,1])))/sum(Θ[j,Ω_star[j]]))
+                else
+                    offers[j,i,1] += φ
+                end # End updating if statement
+            end # i-loop
+        end # j-loop
+
         """ STOPPING CRITERIA"""
-        if
+        if offers_stor[:,:,:,iter] == offers
             println("Wage Equilibrium achieved after $iter iterations")
         # If convergence is not achieved, update offers
         else
-            for j in 1:J
-                for i in 1:I
-                    if (matches[j,i] = 1) & (Ω[j,i] = 1)
-                        
-                    else
-
-                    end # End updating if statement
-                end # i-loop
-            end # j-loop
+            # Store matches/offers from this iteration
+            matches_stor[:,:,iter+1] = matches
+            offers_stor[:,:,:,iter+1] = offers
         end # convergence check
     end # convergence loop
     return offers, Z, matches
@@ -119,18 +133,15 @@ end # wage_eq
 
 """
 MATRICES:
-rents[1,L] -- rents for each location l
-hhs[1,I] -- locations of each household (0 = outside)
-firms[1,J] -- locations of each firm (0 = not operating)
-offers[I,J,2] -- offers by firm j to household i in form (w,n)
-prices[1,J] -- prices of goods by each firm j
-hl[1,L+1] -- distribution of hh's (L+1 = outside)
-fl[1,L+1] -- distribution of firms (L+1 = not operating)
-Θ[I,J] -- Individual household productivity multipliers θ_i
+rents[L] -- rents for each location l
+hhs[I] -- locations of each household (0 = outside)
+firms[J] -- locations of each firm (0 = not operating)
+offers[J,I,2] -- offers by firm j to household i in form (w,n)
+prices[J] -- prices of goods by each firm j
+hl[L+1] -- distribution of hh's (L+1 = outside)
+fl[L+1] -- distribution of firms (L+1 = not operating)
+Θ[J,I] -- Individual household productivity multipliers θ_i
 matches[J,I] -- binary matrix matching households to firms; ie if matches[j,i] = 1, then i works for j
 """
 
-it_tols = [200,1e-5,
-200,1e-5,
-200,1e-5,
-200,1e-5]
+it_tols = [200,1e-5, 200,1e-5, 200,1e-5, 200,1e-5]
